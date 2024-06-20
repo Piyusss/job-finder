c5b56260f67e1625201087d9cf06aa49ea39e936
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-06-20 14:33:29 +0530

    Update index.js

--- src/components/membership/index.js
+++ src/components/membership/index.js
@@ -14,7 +14,7 @@ import { useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
 const stripePromise = loadStripe(
-  "pk_test_51NMv6ZSC6E6fnyMeRIEb9oEXdGRCC9yrBTT4xWHgcjWOuFcqFiAHErvaS50K1hl5t5WJXVGfLLWxvb705IWJhA3300yCcrMnlM"
+  "pk_test_51PTTYEBe0M9xkijNLGI4rTw8TeNIumWUaVO1t3Ftjm1lpnXodUkX6wYSEZzgXf18eZKRgl7bj3Zfa8SxmTon2Cfc00DNUBeCm6"
 );
 
 function Membership({ profileInfo }) {
